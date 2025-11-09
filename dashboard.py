b2d470740449e68b1da2099bb42d14dc566790b2
Mathemusician1729 <tranducmatthew@gmail.com>
2025-11-09 01:13:26 -0700

    mainpage works with csv submission

--- dashboard.py
+++ dashboard.py
@@ -1,38 +1,107 @@
-from flask import Flask, render_template_string, render_template, request
+from flask import Flask, render_template_string, render_template, request, redirect, url_for
 import plotly 
 import io
 import base64
 import matplotlib.pyplot as plt
+import pandas as pd
+import torch
 
-# plot
-fig, ax = plt.subplots()
-x = [1, 2, 3, 4, 5]
-y = [10, 20, 30, 40, 50]
-ax.plot(x,y)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_title('Sample Plot')
+# load AI model
+# model = torch.load()
+# model.eval()
 
-# save chart
-bf = io.BytesIO()
-fig.savefig(bf, format='png')
-plt.close(fig)
-bf.seek(0)
-img_base = base64.b64encode(bf.getvalue()).decode('utf-8')
+# mainpage html (the one that picks up csv)
+html_title = """
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta chaset="UTF-8">
+        <title>EnergyHub - Set Your Energy Goals!</title>
+        <style>
+            h1 {color: #0004ff; 
+                font-size: 65px;
+            }
+            h2 {color: #0004ff; }
+            h3 {color: #0004ff; }
+            body {font-family: 'Inter', sans-serif; text-align: center; padding: 20px; background-color: #cad5e0; }
+            p {color: #0004ff; }
+        </style>
+    </head>
+    <body>
+        <h1><b>Welcome To EnergyHub!</b></h1>
+        <h2>Let's put your home energy to work.</h2>
 
-html = """
+        <!-- File Upload Form -->
+        <form method="POST" action="/plot" enctype="multipart/form-data" class="space-y-4">
+                <p>Please upload a .csv file for the data</p>
+                <input 
+                    type="file" 
+                    name="csv_file" 
+                    accept=".csv" 
+                    required
+                >
+                <button type="submit">
+                    Get Report
+                </button>
+            </form>
+    </body>
+</html>
 
 """
-app = Flask(__name__, template_folder="C:/Users/tranm/Hack-The-Change-2025")
+
+# dashboard html
+html_dashboard = """
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta chaset="UTF-8">
+        <title>EnergyHub - Set Your Energy Goals!</title>
+        <style>
+            h1 {color: #0004ff; 
+                font-size: 65px;
+            }
+            h2 {color: #0004ff; }
+            h3 {color: #0004ff; }
+            body {font-family: 'Inter', sans-serif; text-align: center; padding: 20px; background-color: #cad5e0; }
+            p {color: #0004ff; }
+        </style>
+    </head>
+
+    <body>
+        <h1><b>Get fuckin trolled lolololol</b></h1>
+    </body>
+</html>
+"""
+
+app = Flask(__name__)
 
 @app.route("/")
-def idx():
-    return render_template("ui.html", name="Flask User")
+def homepage():
+    return render_template_string(html_title)
 
-# @app.route("/plot", methods=["POST"])
-# def plot():
-#     if 'csv' not in request.files:
-#         return render_template_string()
+@app.route("/plot", methods=["POST"])
+def plot():
+    if 'csv_file' not in request.files:
+        return render_template_string(html_title, error="No File Found")
+    
+    data = request.files['csv_file']
+    if data.filename == "":
+        return render_template_string(html_title, error="No File Selected")
+    elif not data.filename.endswith(".csv"):
+        return render_template_string(html_title, error="Invalid Filetype")
+    try:
+        df = pd.read_csv(data)
+    except Exception as e:
+        return render_template_string(html_title, error="Couldn't Read csv")
+    
+    # matplotlib code
+
+    return redirect(url_for('dashboard'))
+
+@app.route("/dashboard")
+def dashboard():
+        # display dashboard
+        return render_template_string(html_dashboard)
 
 if __name__ == '__main__':
     app.run(debug=True)
