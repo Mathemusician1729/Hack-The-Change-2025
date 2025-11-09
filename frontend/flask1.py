c797d17b6bb1b1df032cf8c06899cbfb27e4fea9
Mathemusician1729 <tranducmatthew@gmail.com>
2025-11-09 10:29:10 -0700

    graph works

--- frontend/flask1.py
+++ frontend/flask1.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, render_template
-import random
+import random, datetime
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
@@ -8,14 +8,16 @@ app = Flask(__name__)
 def index():
     return render_template('page.html')
 
-
 @app.route('/api/values')
 def get_values():
     global live_int_value
     global data_values
     global power_generated
     global moe
+    global current_time
 
+    
+    current_time = datetime.datetime.now()
 
     power_generated = 28
 
@@ -25,11 +27,16 @@ def get_values():
 
     moe = 52.4
 
-    output = 4.2
+    current_time += datetime.timedelta(hours=1)
+    time = current_time.strftime('%H:%M')
 
-    return jsonify({'value': live_int_value,
-                    "powergen": power_generated,
-                    "moe": moe})
+    # simulate data
+    return jsonify({
+                    "powergen": random.randint(200,400),
+                    'cost': round(random.uniform(0,1.5),2),
+                    "moe": round(random.uniform(0,5),2),
+                    "timestamp": time
+                    })
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
